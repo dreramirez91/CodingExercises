b42bb50731ff8bc6e1df476096d04c9ffc72c68e
dreramirez91 <dreramirez91@gmail.com>
2024-01-22 15:35:55 -0600

    Add alternate solution to valid_parenthesis

--- neet/valid_parenthesis.py
+++ neet/valid_parenthesis.py
@@ -1,16 +1,30 @@
 def isValid(s: str) -> bool:
+    # Solution 1
     hash = {")": "(", "}": "{", "]": "["}
     stack = []
-    for char in s:
-        if char in hash.values():
-            stack.append(char)
+    for c in s:
+        if c in hash.values():
+            stack.append(c)
         else:
             try:
                 open = stack.pop()
-                if open != hash[char]:
+                if open != hash[c]:
                     return False
             except IndexError:
                 return False
+    return False if stack else True
+
+    # Solution 2
+    hash = {")": "(", "}": "{", "]": "["}
+    stack = []
+    for c in s:
+        if c in hash:
+            if stack and stack[-1] == hash[c]:
+                stack.pop()
+            else:
+                return False
+        else:
+            stack.append(c)
     return False if stack else True
 
 
